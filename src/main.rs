d7d612ff1607d3459012c97eb6a13daacebd485a
LOU Xun <aquarhead@gmail.com>
2022-05-24 11:19:42 +0000

    Reply with guild emojis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -10,8 +11,16 @@ struct Handler;
 impl EventHandler for Handler {
   async fn message(&self, ctx: Context, msg: Message) {
     if !msg.author.bot {
-      if let Err(why) = msg.channel_id.say(&ctx.http, ":thinking:").await {
-        println!("Error sending message: {:?}", why);
+      if let Some(guild) = msg.guild_id {
+        if let Ok(emojis) = guild.emojis(&ctx.http).await {
+          let e = {
+            let mut rng = rand::thread_rng();
+            emojis.into_iter().choose(&mut rng).unwrap()
+          };
+          if let Err(why) = msg.channel_id.say(&ctx.http, e).await {
+            println!("Error sending message: {:?}", why);
+          }
+        }
       }
     }
   }
