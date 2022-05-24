5a0e48dd216a3caad2b71a3e62473a3743861f76
LOU Xun <aquarhead@gmail.com>
2022-05-24 10:16:01 +0000

    Init project

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+  async fn message(&self, ctx: Context, msg: Message) {
+    if !msg.author.bot {
+      if let Err(why) = msg.channel_id.say(&ctx.http, ":thinking:").await {
+        println!("Error sending message: {:?}", why);
+      }
+    }
+  }
+
+  async fn ready(&self, _: Context, ready: Ready) {
+    println!("{} is connected!", ready.user.name);
+  }
+}
+
+#[tokio::main]
+async fn main() {
+  let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+  let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+  let mut client = Client::builder(&token, intents)
+    .event_handler(Handler)
+    .await
+    .expect("Err creating client");
+
+  if let Err(why) = client.start().await {
+    println!("Client error: {:?}", why);
+  }
+}
